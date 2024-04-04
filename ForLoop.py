c658adad2655aee886df178cdb73605ac041b1e8
Denis Johnson <166014361+DenisJ99@users.noreply.github.com>
2024-04-04 12:28:51 +0100

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,8 @@
+# for loops = execute a block of code a fixed number of times.
+#                     You can iterate over a range, string, sequence, etc.
+
+for x in range(1, 21):
+   if x == 13:
+       break
+   else:
+       print(x)
